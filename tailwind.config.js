7a24a03d7b7574e01500042215364b0a1ac789b4
William Bradshaw <will.bradshaw50@gmail.com>
2022-05-09 22:03:12 +1000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['"Roboto Serif"', 'ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+    }
+  },
+  plugins: [],
+}
